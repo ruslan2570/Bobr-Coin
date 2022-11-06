26b203ae62b0ada33eb3bc9ec0a2518525f0a2e1
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-11-06 14:11:21 +0300

    Initial commit

---
+++ src/main/java/ruslan2570/bobrcoin/BobrCoinApplication.java
@@ -0,0 +1,13 @@
+package ruslan2570.bobrcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobrCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobrCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruslan2570/bobrcoin/BobrCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan2570.bobrcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobrCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
